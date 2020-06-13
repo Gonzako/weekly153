0d7a9d81db1590301d665a54a62afe4f582bee3d
Gonzako <al341522@uji.es>
2020-06-13 19:28:57 +0200

    Added cinemachine + sprite flipping behaviour

---
+++ Assets/spriteFlipper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spriteFlipper : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D information = null;
+    [SerializeField] SpriteRenderer target = null;
+
+
+    private void FixedUpdate()
+    {
+
+        target.flipX = information.velocity.x < 0;
+
+    }
+
+
+}
