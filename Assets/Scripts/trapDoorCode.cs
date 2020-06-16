f470a5d05d883d66afb7fe833e8ce76ab4389ae8
Gonzako <al341522@uji.es>
2020-06-16 20:37:30 +0200

    Added trapdoor introduction level + player hitbox tweak

--- Assets/Scripts/trapDoorCode.cs
+++ Assets/Scripts/trapDoorCode.cs
@@ -5,26 +5,18 @@ using UnityEngine;
 public class trapDoorCode : MonoBehaviour
 {
 
-    public UnityEngine.Events.UnityEvent onTrapDoorUnlock;
     [SerializeField] public playerStepEvent onPlayerStep = new playerStepEvent();
 
-    private bool isOpen;
 
     public void openTrapDoor()
     {
-        isOpen = true;
-        onTrapDoorUnlock.Invoke();
+        //isOpen = true;
     }
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
-        onPlayerStep.Invoke(isOpen);
-        if (!isOpen)
-        {
-
-        }
+        onPlayerStep.Invoke(false);
     }
 
 }
