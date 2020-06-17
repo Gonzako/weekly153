27f3487a9e5a4428e185de6eb0262c93bc422589
Gonzako <al341522@uji.es>
2020-06-17 21:56:29 +0200

    Added level change code, edited spikes

---
+++ Assets/advanceLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class advanceLevel : MonoBehaviour
+{
+
+    public void loadNextLevel()
+    {
+        StartCoroutine(levelLoadCoroutine());
+    }
+
+    public IEnumerator levelLoadCoroutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+        var idx = SceneManager.GetActiveScene().buildIndex + 1;
+
+        SceneManager.LoadScene(idx, LoadSceneMode.Single);
+    }
+
+}
