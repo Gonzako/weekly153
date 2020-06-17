e87b6b08391e0b818eee06acbd2cf969f6796f82
Bharat Raman <bharat.raman@roundedlearning.com>
2020-06-17 13:29:21 -0700

    Added music and music player
    Also added a short script with a public boolean that depends on the game's state. Music will play if true.

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool in_game; //in_game is a boolean that should be called from a game controller
+    public AudioSource music;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (in_game && !music.isPlaying)
+        {
+            music.Play();
+        }
+        else if(!in_game && music.isPlaying)
+        {
+            music.Stop();
+        }
+    }
+}
