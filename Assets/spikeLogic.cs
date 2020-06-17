3bc0da4f9f15b83f4da6b37ea13fbddc290935ae
Gonzako <al341522@uji.es>
2020-06-17 13:22:26 +0200

    Created sprite prefab + implemented animation + logic

---
+++ Assets/spikeLogic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spikeLogic : MonoBehaviour
+{
+
+    Animator spikeTrigger;
+
+    private const string animTrigger = "StartSpikeUp";
+
+    private bool triggered = false;
+    
+    public void resetTriggeredBool()
+    {
+        triggered = false;
+    }
+
+    private void Start()
+    {
+        spikeTrigger = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!triggered)
+        {
+            spikeTrigger.SetTrigger(animTrigger);
+            triggered = true;
+        }
+    }
+}
